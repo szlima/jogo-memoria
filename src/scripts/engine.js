cf0be8640c106ffdd729fe41caa2228b57beed7f
szlima <suzanelima@outlook.com>
2024-01-10 13:03:38 -0300

    Criando as cartas dinamicamente

---
+++ src/scripts/engine.js
@@ -0,0 +1,22 @@
+const emojis= [
+    "💖", "💖",
+    "👰", "👰",
+    "👨‍👩‍👧‍👦", "👨‍👩‍👧‍👦",
+    "🦢", "🦢",
+    "🌺", "🌺",
+    "🎂", "🎂",
+    "📷", "📷",
+    "⛪", "⛪"
+];
+let openCards= [];
+
+let shuffleEmojis= emojis.sort(() =>
+    (Math.random() > 0.5 ? 2 : -1)
+);
+
+for(let i=0; i<emojis.length; i++){
+    let box= document.createElement("div");
+    box.classList.add("item");
+    box.innerHTML= shuffleEmojis[i];
+    document.querySelector(".game").appendChild(box);
+}
