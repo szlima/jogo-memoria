f3a817dd221daa7d879c474c88563f8216fb738b
szlima <suzanelima@outlook.com>
2024-01-11 10:07:25 -0300

    Possibilitando a abertura de pares de cartas

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -18,5 +18,21 @@ for(let i=0; i<emojis.length; i++){
     let box= document.createElement("div");
     box.classList.add("item");
     box.innerHTML= shuffleEmojis[i];
+    box.onclick= handleClick;
     document.querySelector(".game").appendChild(box);
+}
+
+function handleClick(){
+    if(openCards.length < 2){
+        this.classList.add("boxOpen");
+        openCards.push(this);
+    }
+
+    if(openCards.length == 2){
+        setTimeout(checkMatch, 500);
+    }
+}
+
+function checkMatch(){
+
 }
