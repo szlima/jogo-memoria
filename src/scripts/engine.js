904c3973ebbb7018e09e35be680dea02dbc73525
szlima <suzanelima@outlook.com>
2024-01-11 11:48:38 -0300

    Definindo a condição de vitória

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -28,11 +28,21 @@ function handleClick(){
         openCards.push(this);
     }
 
-    if(openCards.length == 2){
+    if(openCards.length == 2)
         setTimeout(checkMatch, 500);
-    }
 }
 
 function checkMatch(){
+    if(openCards[0].innerHTML === openCards[1].innerHTML){
+        openCards[0].classList.add("boxMatch");
+        openCards[1].classList.add("boxMatch");
+    }else{
+        openCards[0].classList.remove("boxOpen");
+        openCards[1].classList.remove("boxOpen");
+    }
 
+    openCards= [];
+
+    if(document.querySelectorAll(".boxMatch").length == emojis.length)
+        alert("Você venceu!");
 }
